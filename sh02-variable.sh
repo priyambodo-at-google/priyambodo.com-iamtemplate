8cd5f25cc3fc90c4b02c1158ec509a9512256d0c
Doddi Priyambodo <doddi@bicarait.com>
2025-08-06 22:39:31 +0700

    Update sh02-variable.sh

--- sh02-variable.sh
+++ sh02-variable.sh
@@ -1,14 +1,15 @@
+export GCP_REGION='us-central1'
 export PROJECT_ID=$(gcloud config get project)
 export PROJECT_NUMBER=$(gcloud projects describe ${PROJECT_ID} --format="value(projectNumber)")
 export SERVICE_ACCOUNT_NAME=$(gcloud compute project-info describe --format="value(defaultServiceAccount)")
 export GOOGLE_CLOUD_PROJECT=$(gcloud config get project)
 export GOOGLE_GENAI_USE_VERTEXAI=TRUE
-export GOOGLE_CLOUD_LOCATION="us-central1"
+export GOOGLE_CLOUD_LOCATION=$GCP_REGION
 
-export GCP_REGION='us-central1'
 export AR_REPO='priyambodocom-artifactregistry'  
 export SERVICE_NAME='changeme-priyambodo-com'
 
 echo $GCP_REGION
+echo $PROJECT_ID
 echo $AR_REPO
 echo $SERVICE_NAME
