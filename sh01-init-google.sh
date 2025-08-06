9631db836e861aef4f8cede8d43aba1560fb090d
Doddi Priyambodo <doddi@bicarait.com>
2025-08-06 22:31:13 +0700

    Update sh01-init-google.sh

--- sh01-init-google.sh
+++ sh01-init-google.sh
@@ -1,3 +1,4 @@
+gcloud init
 gcloud auth login
 gcloud auth application-default login
 
@@ -6,9 +7,14 @@ export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format="value(pro
 
 gcloud config set project $PROJECT_ID
 gcloud projects describe $PROJECT_ID --format="value(projectNumber)"
+gcloud auth application-default set-quota-project $PROJECT_ID
+gcloud config set billing/quota_project $PROJECT_ID
 gcloud config list project
+gcloud auth list
 
-gcloud services enable \
+gcloud services list --available
+
+gcloud services enable bigquery.googleapis.com \
                        compute.googleapis.com \
                        cloudresourcemanager.googleapis.com \
                        servicenetworking.googleapis.com \
@@ -20,4 +26,5 @@ gcloud services enable run.googleapis.com \
                        artifactregistry.googleapis.com \
                        iam.googleapis.com \
                        secretmanager.googleapis.com \
-                       storage.googleapis.com
+                       storage.googleapis.com \
+                       pubsub.googleapis.com
